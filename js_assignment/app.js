3dcf62f3beddb1305ea434597efcf2a1536440ee
Kazu <114047336+WaiHlyanZ@users.noreply.github.com>
2024-05-15 10:41:55 +0630

    Add regex to validate inputs

--- js_assignment/app.js
+++ js_assignment/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 function inputValid(name, age) {
@@ -34,3 +35,41 @@ function addRow() {
         alert("Please, enter valid Name and Age!");
     }
 }
+=======
+console.log("Test Success");
+
+function inputValid(name, age) {
+    // Pattern to check, start with letter or underscore
+    let pattern = /^\w+\s?\w+$/mi; 
+    return pattern.test(name) && 0 < age && age < 131;
+}
+
+// To count row number
+let count = 1;
+function addRow() {
+    // Assigning element's values to variables
+    let nameInput = document.getElementById('name').value;
+    let ageInput = document.getElementById('age').value;
+    // Enforce to enter some values
+    if (nameInput && ageInput && inputValid(nameInput, ageInput)) {
+        // Initializing table's things
+        const table = document.getElementById("info");
+        const newRow = table.insertRow();
+        const cell1 = newRow.insertCell(0);
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
+
+        // Assigning values to table's cells
+        cell1.textContent = count++;
+        cell2.textContent = nameInput;
+        cell3.textContent = ageInput;
+
+        // Cleaning inputs' values
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+
+    } else {
+        alert("Please, enter valid Name and Age!");
+    }
+}
+>>>>>>> 39f4ad1 (Add regex to validate inputs)
