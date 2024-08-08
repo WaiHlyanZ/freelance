5d38041adae5e3a82a6e557eed740e5fc435450c
Kazu <hlyanw@yahoo.com>
2024-08-08 09:43:35 +0630

    Finished assg1

---
+++ sql_assignments/assignment1/create_tbs.sql
@@ -0,0 +1,26 @@
+
+-- creating sale_people table
+CREATE TABLE sale_people (
+    sale_number INT,
+    sale_name VARCHAR(50),
+    city VARCHAR(50),
+    c_omm DECIMAL(3, 2)
+)
+
+-- creating customers table
+CREATE TABLE customers (
+    c_number INT,
+    c_name VARCHAR(50),
+    city VARCHAR(50),
+    rating INT,
+    s_num INT
+)
+
+-- creating orders table
+CREATE TABLE orders (
+    order_number INT,
+    amount DECIMAL(7, 2),
+    ordered_date DATE,
+    c_num INT,
+    s_num INT
+)
