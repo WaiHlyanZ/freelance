e054ed4cce948c4b7769f4043aae9370541d606e
Kazu <hlyanw@yahoo.com>
2024-08-11 14:51:18 +0630

    Finished assignment 4

---
+++ sql_assignments/assignment4/analyze.sql
@@ -0,0 +1,27 @@
+USE shop_data;
+
+-- Task 1
+SELECT COUNT(*)
+FROM orders
+WHERE s_num = 1001 AND amount > 1500;
+
+-- Task 2
+SELECT COUNT(c_name)
+FROM customers
+WHERE city = "London";
+
+-- Task 3
+SELECT *
+FROM orders
+ORDER BY amount
+LIMIT 1;
+
+-- Task 4
+SELECT ordered_date, MAX(amount) AS highest_amount
+FROM orders
+GROUP BY ordered_date;
+
+-- Task 5
+SELECT rating, c_name, c_number
+FROM customers
+ORDER BY rating DESC;
